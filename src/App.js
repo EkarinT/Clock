13e9bd3b6213f865320284b87b8858be2d523f19
ekarin <ekarin.thin@outlook.com>
2019-09-24 14:28:11 +0700

    Analog Clock

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Clock from "react-clock";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AnalogClock extends Component {
+  state = {
+    date: new Date()
+  };
+
+  componentDidMount() {
+    setInterval(() => this.setState({ date: new Date() }), 1000);
+  }
+
+  render() {
+    return (
+      <div>
+         <header className="App-header">
+        <Clock className="clock" size='720' renderNumbers={true} value={this.state.date}></Clock>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default AnalogClock;
